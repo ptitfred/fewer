9351d574d434144f85f8ca4ddfb96f3e0c90abed
Frédéric Menou <frederic.menou@gmail.com>
2012-06-01 23:02:16 +0200

    More PLS tests

--- src/test/java/org/kercoin/android/fewer/PLSParserTest.java
+++ src/test/java/org/kercoin/android/fewer/PLSParserTest.java
@@ -30,4 +30,22 @@ public class PLSParserTest {
         assertThat(channels[0].getStreams()[0].getOptions()).isNotNull().hasSize(0);
         assertThat(channels[1].getStreams()[0].getSource()).isEqualTo("http://example.com/song.mp3?bitrate=96kbps");
     }
+
+    @Test
+    public void test2() {
+        // given
+        load("playlist-di.pls");
+
+        // when
+        Channel[] channels = parser.parse();
+
+        // then
+        assertThat(channels).isNotNull().hasSize(1);
+        assertThat(channels[0].getKey()).isNull();
+        assertThat(channels[0].getName()).isEqualTo("Digitally Imported - Hardcore");
+        assertThat(channels[0].getStreams()).hasSize(1);
+        assertThat(channels[0].getStreams()[0].getName()).isEqualTo("Digitally Imported - Hardcore");
+        assertThat(channels[0].getStreams()[0].getSource()).isEqualTo("http://u11aw.di.fm:80/di_hardcore");
+        assertThat(channels[0].getStreams()[0].getOptions()).isNotNull().hasSize(0);
+    }
 }
---
+++ src/test/resources/org/kercoin/android/fewer/playlist-di.pls
@@ -0,0 +1,6 @@
+[playlist]
+NumberOfEntries=1
+File1=http://u11aw.di.fm:80/di_hardcore
+Title1=Digitally Imported - Hardcore
+Length1=-1
+Version=2
